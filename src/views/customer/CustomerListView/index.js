b0719008d864831f0e61335df0c981f6924c228a
adilmoumni <49393758+adilmoumni@users.noreply.github.com>
2021-03-17 15:26:20 +0100

    update relative path

--- src/views/customer/CustomerListView/index.js
+++ src/views/customer/CustomerListView/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Box, Container, makeStyles } from '@material-ui/core';
-import Page from 'src/components/Page';
+import Page from '../../../components/Page';
 import Results from './Results';
 import Toolbar from './Toolbar';
 import data from './data';
